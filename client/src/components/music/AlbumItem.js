c8d547ddb20ca5616fc46e2d6b9284203a02ade9
rokas <augunasr@gmail.com>
2021-12-09 15:46:53 +0000

    client

--- client/src/components/music/AlbumItem.js
+++ client/src/components/music/AlbumItem.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
+import Heart from "react-heart";
 
 import Modal from "../shared/Modal";
 import "./AlbumItem.css";
 
 export default function AlbumItem(props) {
   const [showModal, setShowModal] = useState(false);
+  const [active, setActive] = useState(false);
 
   // To open modal
   const openModal = () => {
@@ -13,20 +15,34 @@ export default function AlbumItem(props) {
 
   return (
     <div>
-      <div className="album-item-index">{props.index}</div>
-      <li className="album-item-wrapper">
-        <div className="album-item-container">
-          <div className="album-item-image">
-            <img src={props.image} alt="" />
+      <div className="album-item-main">
+        <div className="album-item-index">{props.index}</div>
+        <li className="album-item-wrapper">
+          <div className="album-item-container">
+            <div className="album-item-image">
+              <img src={props.image} alt="" />
+            </div>
+            <div className="album-item-text-container">
+              <div className="album-item-album-name">{props.albumName}</div>
+              <div className="album-item-artist">{props.artist}</div>
+            </div>
+            <div className="album-item-heart-preview-container">
+              <div className="album-item-heart">
+                <Heart isActive={active} onClick={() => setActive(!active)} />
+              </div>
+              <button
+                onClick={openModal}
+                className=""
+                type="button"
+                className="album-item-btn"
+              >
+                Preview
+              </button>
+            </div>
           </div>
-          <div className="album-item-text-container">
-            <div className="album-item-album-name">{props.albumName}</div>
-            <div className="album-item-artist">{props.artist}</div>
-          </div>
-          <button onClick={openModal} className="" type="button"></button>
-        </div>
-      </li>
-      {showModal && <Modal setShowModal={setShowModal} props={props} />}
+        </li>
+        {showModal && <Modal setShowModal={setShowModal} props={props} />}
+      </div>
     </div>
   );
 }
--- client/src/components/music/AlbumList.js
+++ client/src/components/music/AlbumList.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { v4 as uuid_v4 } from "uuid";
+import SearchIcon from "@mui/icons-material/Search";
 
 import AlbumItem from "./AlbumItem";
 import "./AlbumList.css";
@@ -23,23 +24,36 @@ export default function AlbumList({ loadedAlbums, code }) {
 
   return (
     <div>
-      <input type="text" onChange={(e) => setQuery(e.target.value)} />
-      <ul className="album-list">
-        {filteredItems.map((album, index) => (
-          <AlbumItem
-            index={index + 1}
-            id={album.id}
-            key={uuid_v4()}
-            artist={album["im:artist"].label}
-            albumName={album["im:name"].label}
-            category={album.category.attributes.label}
-            year={album["im:releaseDate"].label.slice(0, 4)}
-            image={album["im:image"][2].label}
-            songCount={album["im:itemCount"].label}
-            code={code}
-          />
-        ))}
-      </ul>
+      <div className="album-list-search-container">
+        <div className="album-list-container">
+          <h1 className="album-list-title">Top Albums</h1>
+          <div className="search-box">
+            <input
+              className="search-text"
+              type="text"
+              placeholder="Search"
+              onChange={(e) => setQuery(e.target.value)}
+            />
+            <SearchIcon className="search-btn" style={{ fontSize: 40 }} />
+          </div>
+        </div>
+        <ul className="album-list">
+          {filteredItems.map((album, index) => (
+            <AlbumItem
+              index={index + 1}
+              id={album.id}
+              key={uuid_v4()}
+              artist={album["im:artist"].label}
+              albumName={album["im:name"].label}
+              category={album.category.attributes.label}
+              year={album["im:releaseDate"].label.slice(0, 4)}
+              image={album["im:image"][2].label}
+              songCount={album["im:itemCount"].label}
+              code={code}
+            />
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
